9e3454d89ab19312bd2261bc0e554c37c59ec62e
Kai Stierand <kai@stierand.com>
2019-05-10 10:33:04 +0200

    Initial cmake Project

---
+++ Novastor_Probeaufgabe/Novastor_Probeaufgabe.cpp
@@ -0,0 +1,12 @@
+﻿// Novastor_Probeaufgabe.cpp : Defines the entry point for the application.
+//
+
+#include "Novastor_Probeaufgabe.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
