763fa2b3320058978e4c9cf299897f0be7fd2ead
Jens Windmüller <jens.windmueller@swplusplus.de>
2019-05-11 14:37:36 +0200

    add unit test executable target and minimalistic ctest tests to check if applications are runnable.

---
+++ Novastor_Probeaufgabe_UT.cpp
@@ -0,0 +1,7 @@
+#define BOOST_TEST_MODULE Novastor Probeaufgabe UT
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(free_test_function)
+{
+    BOOST_TEST(true /* test assertion */);
+}
