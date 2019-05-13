87029376d7aa40a5d6094ec3569efb86d349c2b7
Jens Windmüller <jens.windmueller@swplusplus.de>
2019-05-13 16:24:20 +0200

    add unit tests for LocalFilesystemWorker

--- test/LocalFilesystemWorker_UT.cpp
+++ test/LocalFilesystemWorker_UT.cpp
@@ -1,18 +1,83 @@
 #include <boost/test/unit_test.hpp>
 #include <filesystem>
 #include "Testdata.h"
+#include "worker/LocalFilesystemWorker.h"
 
 namespace fs = std::filesystem;
 
-BOOST_AUTO_TEST_SUITE(worker)
+struct TestFixture
+{
+	WorkerBase::WorkQueue workQueue;
+	FilesystemEntry::Queue outQueue;
+};
 
-BOOST_AUTO_TEST_CASE(scanFolder0Files)
+void CompareEntries(const FilesystemEntry& e1, const FilesystemEntry& e2)
+{
+	BOOST_TEST(e1.m_filename == e2.m_filename);
+	BOOST_TEST(e1.m_sizeInBytes == e2.m_sizeInBytes);
+}
+
+BOOST_AUTO_TEST_SUITE(worker, *boost::unit_test::timeout(10))
+
+BOOST_FIXTURE_TEST_CASE(scanFolder0Files, TestFixture)
 {
 	fs::path folderPath = test_base_dir / "testfolder/0";
 
+	LocalFilesystemWorker worker{ folderPath };
+
+	BOOST_CHECK_NO_THROW(worker.Work(WorkerBase::WorkQueueV{ workQueue }, WorkerBase::OutQueueV{ outQueue }));
+	BOOST_TEST(workQueue.empty());
+	BOOST_TEST(outQueue.empty());
+}
+
+BOOST_FIXTURE_TEST_CASE(scanFolder1File, TestFixture)
+{
+	fs::path folderPath = test_base_dir / "testfolder/1";
+
+	LocalFilesystemWorker worker{ folderPath };
+
+	BOOST_CHECK_NO_THROW(worker.Work(WorkerBase::WorkQueueV{ workQueue }, WorkerBase::OutQueueV{ outQueue }));
+	BOOST_TEST(workQueue.empty());
+	BOOST_TEST_REQUIRE(outQueue.size() == 1);
+
+	CompareEntries({ (folderPath / "1.txt").string(), fs::file_time_type {}, 0 }, outQueue.pull());
+}
+
+BOOST_FIXTURE_TEST_CASE(scanFolder2Files, TestFixture)
+{
+	fs::path folderPath = test_base_dir / "testfolder/2";
+
+	LocalFilesystemWorker worker{ folderPath };
+
+	BOOST_CHECK_NO_THROW(worker.Work(WorkerBase::WorkQueueV{ workQueue }, WorkerBase::OutQueueV{ outQueue }));
+	BOOST_TEST(workQueue.empty());
+	BOOST_TEST_REQUIRE(outQueue.size() == 2);
+
+	CompareEntries({ (folderPath / "1").string(), fs::file_time_type {}, 0 }, outQueue.pull());
+	CompareEntries({ (folderPath / "2.2").string(), fs::file_time_type {}, 0 }, outQueue.pull());
+}
 
 
-	BOOST_TEST(true /* test assertion */);
+BOOST_FIXTURE_TEST_CASE(scanFolderWithSubfolders, TestFixture)
+{
+	fs::path folderPath = test_base_dir / "testfolder";
+
+	LocalFilesystemWorker worker{ folderPath };
+
+	BOOST_CHECK_NO_THROW(worker.Work(WorkerBase::WorkQueueV{ workQueue }, WorkerBase::OutQueueV{ outQueue }));
+	BOOST_TEST_REQUIRE(workQueue.size() == 3);
+	BOOST_TEST(outQueue.empty());
+
+	while (!workQueue.empty())
+	{
+		auto w = workQueue.pull();
+		BOOST_CHECK_NO_THROW(w->Work(WorkerBase::WorkQueueV{ workQueue }, WorkerBase::OutQueueV{ outQueue }));
+	}
+
+	BOOST_TEST_REQUIRE(outQueue.size() == 3);
+	CompareEntries({ (folderPath / "1.txt").string(), fs::file_time_type {}, 0 }, outQueue.pull());
+	CompareEntries({ (folderPath / "1").string(), fs::file_time_type {}, 0 }, outQueue.pull());
+	CompareEntries({ (folderPath / "2.2").string(), fs::file_time_type {}, 0 }, outQueue.pull());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
