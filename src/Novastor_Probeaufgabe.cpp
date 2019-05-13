e24d9d7000779c75c2b0957dab8fd1054f720aca
Kai Stierand <kai@stierand.com>
2019-05-13 21:10:36 +0200

    Parse command line options

--- src/Novastor_Probeaufgabe.cpp
+++ src/Novastor_Probeaufgabe.cpp
@@ -1,12 +1,62 @@
-﻿// Novastor_Probeaufgabe.cpp : Defines the entry point for the application.
-//
+﻿#include <boost/program_options.hpp>
+#include <filesystem>
+#include <iostream>
+#include <string>
+#include <thread>
 
-#include "Novastor_Probeaufgabe.h"
-
-using namespace std;
-
-int main()
+class Novastore_Probeaufgabe
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+public:
+	int Run(int argc, char **argv)
+	{
+		using namespace std::string_view_literals;
+		namespace po = boost::program_options;
+
+		unsigned int cpus = std::thread::hardware_concurrency();
+		bool help = false;
+		std::vector<std::string> paths;
+		// Declare the supported options.
+		po::options_description desc("Options");
+		desc.add_options()
+			("help,h", "this help message")
+			("threads,t", po::value(&cpus)->default_value(cpus), "set number of threads to use");
+		po::options_description hidden("hidden options");
+		hidden.add_options()
+			("path", po::value(&paths), "path to search");
+
+		po::positional_options_description posOpts;
+		posOpts.add("path", -1);
+
+		po::options_description cmdLineOptions;
+		cmdLineOptions.add(desc).add(hidden);
+
+		po::variables_map vm;
+		po::command_line_parser parser(argc, argv);
+		parser.options(cmdLineOptions)
+			.positional(posOpts);
+		po::store(parser.run(), vm);
+		po::notify(vm);
+
+		if (vm.count("help") > 0)
+		{
+			std::filesystem::path path(argv[0]);
+			std::cout << "Synopsis: "sv << path.filename() << " [Options] [paths]\n"sv << std::endl;
+			std::cout << desc << "\n";
+			return EXIT_SUCCESS;
+		}
+
+		std::cout << "Using " << cpus << " threads" << std::endl;
+		for (const auto& p : paths)
+		{
+			std::cout << "path: " << p << std::endl;
+		}
+
+		return EXIT_SUCCESS;
+	}
+};
+
+int main(int argc, char **argv)
+{
+	Novastore_Probeaufgabe app;
+	return app.Run(argc, argv);
 }
--- src/Novastor_Probeaufgabe.h
+++
@@ -1,8 +0,0 @@
-﻿// Novastor_Probeaufgabe.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Reference additional headers your program requires here.
